327ca0b2355c1aa18a48cd2f39ee9f347af179c5
David Campbell <dcampbell24@gmail.com>
2024-03-27 17:41:59 -0400

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Write;
+
 use bme280::{i2c::BME280, Measurements};
 use embedded_hal::delay::DelayNs;
 use linux_embedded_hal::{Delay, I2CError, I2cdev};
@@ -32,8 +34,8 @@ fn main() {
         let measurements = bme280.measure(&mut delay).unwrap();
         // let r: f64 = rng.gen();
 
-        print_measurements(&measurements);
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(0, 0));
+        let str = write_measurements(&measurements);
+        print!("{}{}{}", termion::clear::All, termion::cursor::Goto(0, 0), str);
 
         humidity.set(measurements.humidity);
         pressure.set(measurements.pressure);
@@ -45,14 +47,16 @@ fn main() {
     }
 }
 
-fn print_measurements(measurements: &Measurements<I2CError>) {
+fn write_measurements(measurements: &Measurements<I2CError>) -> String {
     // Change to round_ties_even when stabilized.
     let rh = (measurements.humidity * 10.0).round() / 10.0;
     let t1 = (measurements.temperature * 10.0).round() / 10.0;
     let mut t2 = (measurements.temperature * 1.8) + 32.0;
     t2 = (t2 * 100.0).round() / 100.0;
 
-    println!("Pressure: {} ± 100 pascals", measurements.pressure.round());
-    println!("Relative Humidity: {rh:.1} ± 3 %");
-    println!("Temperature: {t1:.1} ± 1 °C  {t2:.2} ± 1.8 °F");
+    let mut out = String::new();
+    writeln!(out, "Pressure: {} ± 100 pascals", measurements.pressure.round()).unwrap();
+    writeln!(out, "Relative Humidity: {rh:.1} ± 3 %").unwrap();
+    writeln!(out, "Temperature: {t1:.1} ± 1 °C  {t2:.2} ± 1.8 °F").unwrap();
+    out
 }
