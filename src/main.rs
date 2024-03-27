4af5990ed3bb2bde5adf9c9ba1b9e14972725f70
David Campbell <dcampbell24@gmail.com>
2024-03-27 00:32:28 -0400

    Serve to prometheus.

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,53 @@
-use std::fmt::{self, Write};
-
 use bme280::{i2c::BME280, Measurements};
 use embedded_hal::delay::DelayNs;
 use linux_embedded_hal::{Delay, I2CError, I2cdev};
+use metrics::gauge;
+use metrics_exporter_prometheus::PrometheusBuilder;
+// use rand::Rng;
+
+// use std::{thread, time};
 
 fn main() {
-    let mut bme280 = BME280Prometheus::init();
+    let builder = PrometheusBuilder::new();
+    // Defaults to enabled, listening at 0.0.0.0:9000
+    builder
+        .install()
+        .expect("failed to install recorder/exporter");
+    let temperature = gauge!("temperature");
+
+    // Using Linux I2C Bus #1 in this example.
+    let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
+    // Initialize the BME280 using the primary I2C address 0x76.
+    let mut bme280 = BME280::new_primary(i2c_bus);
+
+    let mut delay = Delay;
+    bme280.init(&mut delay).unwrap();
+
+    // let mut rng = rand::thread_rng();
+    // let one_second = time::Duration::from_millis(1_000);
     loop {
-        bme280.read();
-        print!(
-            "{}{}{}",
-            termion::clear::All,
-            termion::cursor::Goto(1, 1),
-            bme280
-        );
-        bme280.wait_ms(1_000);
+        let measurements = bme280.measure(&mut delay).unwrap();
+        // let r: f64 = rng.gen();
+
+        print_measurements(&measurements);
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(0, 0));
+
+        temperature.set(measurements.temperature);
+        // temperature.set(r);
+
+        delay.delay_ms(1_000);
+        // thread::sleep(one_second);
     }
 }
 
-struct BME280Prometheus {
-    bme280: BME280<I2cdev>,
-    delay: Delay,
-    measurements: Option<Result<Measurements<I2CError>, bme280::Error<I2CError>>>,
-}
+fn print_measurements(measurements: &Measurements<I2CError>) {
+    // Change to round_ties_even when stabilized.
+    let rh = (measurements.humidity * 10.0).round() / 10.0;
+    let t1 = (measurements.temperature * 10.0).round() / 10.0;
+    let mut t2 = (measurements.temperature * 1.8) + 32.0;
+    t2 = (t2 * 100.0).round() / 100.0;
 
-impl BME280Prometheus {
-    fn init() -> Self {
-        // Using Linux I2C Bus #1 in this example.
-        let i2c_bus = I2cdev::new("/dev/i2c-1").unwrap();
-        let mut delay = Delay;
-
-        // Initialize the BME280 using the primary I2C address 0x76.
-        let mut bme280 = BME280::new_primary(i2c_bus);
-        bme280.init(&mut delay).unwrap();
-
-        BME280Prometheus {
-            bme280,
-            delay,
-            measurements: None,
-        }
-    }
-
-    fn read(&mut self) {
-        self.measurements = Some(self.bme280.measure(&mut self.delay));
-    }
-
-    fn wait_ms(&mut self, ms: u32) {
-        self.delay.delay_ms(ms);
-    }
-}
-
-impl fmt::Display for BME280Prometheus {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut out = String::new();
-        match &self.measurements {
-            Some(measurements) => {
-                match measurements {
-                    // Change to round_ties_even when stabilized.
-                    Ok(measurements) => {
-                        let rh = (measurements.humidity * 10.0).round() / 10.0;
-                        let t1 = (measurements.temperature * 10.0).round() / 10.0;
-                        let mut t2 = (measurements.temperature * 1.8) + 32.0;
-                        t2 = (t2 * 100.0).round() / 100.0;
-
-                        writeln!(out, "Relative Humidity: {rh:.1} ± 3 %")?;
-                        writeln!(out, "Temperature: {t1:.1} ± 1 °C  {t2:.2} ± 1.8 °F")?;
-                        writeln!(
-                            out,
-                            "Pressure: {} ± 100 pascals",
-                            measurements.pressure.round()
-                        )?;
-                    }
-                    Err(e) => writeln!(out, "{e:?}")?,
-                }
-            }
-            None => writeln!(out, "no readings taken yet")?,
-        }
-
-        write!(f, "{out}")
-    }
+    println!("Pressure: {} ± 100 pascals", measurements.pressure.round());
+    println!("Relative Humidity: {rh:.1} ± 3 %");
+    println!("Temperature: {t1:.1} ± 1 °C  {t2:.2} ± 1.8 °F");
 }
